2110fbf72ef6f3cc70e7c78b1f52f9ba878b9583
dev-level-pro <dev.level.pro@gmail.com>
2025-03-17 16:41:31 +0000

    Initial commit - Portfolio Espoir SOSSOU

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
 
     <title>🧑‍💻🚀Portfolio-Espoir-SOSSOU</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}" />
+    <link rel="stylesheet" href="/assets/css/style.css" />
     <link rel="stylesheet" href="{{ asset('assets/vendors/linericon/style.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendors/owl-carousel/owl.carousel.min.css') }}" />
