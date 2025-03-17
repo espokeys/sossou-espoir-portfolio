141249756328d529b3cb98ae8a8e329a15606266
dev-level-pro <dev.level.pro@gmail.com>
2025-03-17 11:48:20 +0000

    Initial commit - Portfolio Espoir SOSSOU

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,9 +5,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="icon" href="{{ asset('assets/img/nessgood_logo_favicon.png') }}" style="border-radius: 5px;"
-        type="image/png" />
-    <title>Satner Portfolio</title>
+
+    <title>🧑‍💻🚀Portfolio---Espoir-SOSSOU</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/vendors/linericon/style.css') }}" />
