999ef2f045613fe2d10eaabdce93f3ad07003a56
dev-level-pro <dev.level.pro@gmail.com>
2025-03-17 17:17:37 +0000

    Initial commit - Portfolio Espoir SOSSOU

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,14 +7,14 @@
 
     <title>🧑‍💻🚀Portfolio-Espoir-SOSSOU</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/vendors/linericon/style.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/vendors/owl-carousel/owl.carousel.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/vendors/nice-select/css/nice-select.css') }}" />
+    <link rel="stylesheet" href="{{ url('assets/css/bootstrap.css') }}" />
+    <link rel="stylesheet" href="{{ url('assets/vendors/linericon/style.css') }}" />
+    <link rel="stylesheet" href="{{ url('assets/css/font-awesome.min.css') }}" />
+    <link rel="stylesheet" href="{{ url('assets/vendors/owl-carousel/owl.carousel.min.css') }}" />
+    <link rel="stylesheet" href="{{ url('assets/css/magnific-popup.css') }}" />
+    <link rel="stylesheet" href="{{ url('assets/vendors/nice-select/css/nice-select.css') }}" />
     <!-- main css -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}" />
 
     <style>
         .logo_h img {
